ba6b7c5214f68a6982b53b17b4ef1a1a30a8b28a
Julian van der Giesen <julianvandergiesen@hotmail.com>
2016-08-11 11:23:34 +0200

    Like posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
     
     devise_for :users
-    resources :posts
+    
+    resources :posts  do
+    resources :likes
+    end
     
     get 'user_posts/:user_id' => 'posts#user', as: :user_posts
     
